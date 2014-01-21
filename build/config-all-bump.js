a3e73a44f2c20e0a0e8ecc9cf287ceb183ecd1e2
Scott Rippey <scott.william.rippey@gmail.com>
2014-01-20 20:59:43 -0800

    Bower - Added Bower

--- build/config-all-bump.js
+++ build/config-all-bump.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   grunt.mergeConfig({
     bump: {
       options: {
+        files: [ 'package.json', 'bower.json' ],
         commit: false, createTag: false, push: false
       }
     }
