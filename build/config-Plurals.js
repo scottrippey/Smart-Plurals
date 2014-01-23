7b65f7c94029cdd2b7bd3368e8571da2b0800db6
Scott Rippey <scott.william.rippey@gmail.com>
2014-01-22 22:36:43 -0800

    Grunt - inlined paths

--- build/config-Plurals.js
+++ build/config-Plurals.js
@@ -2,49 +2,44 @@ module.exports = function(grunt) {
 
   grunt.registerTask('Plurals', [ 'concat:PLURALS', 'uglify:PLURALS', 'jshint:PLURALS', 'jasmine:PLURALS' ]);
 
-  var src = 'src/Smart/Plurals'
-    , dest = 'dist'
-    , releases = 'releases'
-    , test = 'test/Plurals'
-    ;
   var englishOnly = [
-    src + '/Smart.Plurals.core.js'
-    , src + '/Smart.Plurals.languages-english.js'
+    'src/Smart/Plurals/Smart.Plurals.core.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-english.js'
   ];
   var allFiles = [
-    src + '/Smart.Plurals.core.js'
-    , src + '/Smart.Plurals.languages-english.js'
-    , src + '/Smart.Plurals.languages-czech.js'
-    , src + '/Smart.Plurals.languages-french.js'
-    , src + '/Smart.Plurals.languages-irish.js'
-    , src + '/Smart.Plurals.languages-latvian.js'
-    , src + '/Smart.Plurals.languages-lithuanian.js'
-    , src + '/Smart.Plurals.languages-polish.js'
-    , src + '/Smart.Plurals.languages-romanian.js'
-    , src + '/Smart.Plurals.languages-russian.js'
-    , src + '/Smart.Plurals.languages-slovenian.js'
+    'src/Smart/Plurals/Smart.Plurals.core.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-english.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-czech.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-french.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-irish.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-latvian.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-lithuanian.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-polish.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-romanian.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-russian.js'
+    , 'src/Smart/Plurals/Smart.Plurals.languages-slovenian.js'
   ];
 
   var allTests = [
-    test + '/**/*.js'
+    'test/Plurals/Spec-Plurals.js'
   ];
 
   grunt.mergeConfig({
     concat: {
       'PLURALS': {
         files: [
-          { dest: dest + '/Smart.Plurals.all.js', src: allFiles }
-          , { dest: dest + '/Smart.Plurals.en.js', src: englishOnly }
+          { dest: 'dist/Smart.Plurals.all.js', src: allFiles }
+          , { dest: 'dist/Smart.Plurals.en.js', src: englishOnly }
         ]
       }
       ,
       // Just copy to the releases folder:
       'PLURALS-RELEASE': {
         files: [
-          { dest: releases + '/v<%= pkg.version %>/Smart.Plurals.all-<%= pkg.version %>.js', src: dest + '/Smart.Plurals.all.js' }
-          ,{ dest: releases + '/v<%= pkg.version %>/Smart.Plurals.all-<%= pkg.version %>.min.js', src: dest + '/Smart.Plurals.all.min.js' }
-          ,{ dest: releases + '/v<%= pkg.version %>/Smart.Plurals.en-<%= pkg.version %>.js', src: dest + '/Smart.Plurals.en.js' }
-          ,{ dest: releases + '/v<%= pkg.version %>/Smart.Plurals.en-<%= pkg.version %>.min.js', src: dest + '/Smart.Plurals.en.min.js' }
+          { dest: 'releases/v<%= pkg.version %>/Smart.Plurals.all-<%= pkg.version %>.js', src: 'dist/Smart.Plurals.all.js' }
+          ,{ dest: 'releases/v<%= pkg.version %>/Smart.Plurals.all-<%= pkg.version %>.min.js', src: 'dist/Smart.Plurals.all.min.js' }
+          ,{ dest: 'releases/v<%= pkg.version %>/Smart.Plurals.en-<%= pkg.version %>.js', src: 'dist/Smart.Plurals.en.js' }
+          ,{ dest: 'releases/v<%= pkg.version %>/Smart.Plurals.en-<%= pkg.version %>.min.js', src: 'dist/Smart.Plurals.en.min.js' }
         ]
       }
     }
@@ -52,8 +47,8 @@ module.exports = function(grunt) {
     uglify: {
       'PLURALS': {
         files: [
-          { dest: dest + '/Smart.Plurals.all.min.js', src: allFiles }
-          , { dest: dest + '/Smart.Plurals.en.min.js', src: englishOnly }
+          { dest: 'dist/Smart.Plurals.all.min.js', src: allFiles }
+          , { dest: 'dist/Smart.Plurals.en.min.js', src: englishOnly }
         ]
       }
     }
