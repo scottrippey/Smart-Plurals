a892e722a10344a3871040a133724282e7158955
Scott Rippey <scott.william.rippey@gmail.com>
2014-02-22 15:53:09 -0800

    Tests - Added AngularJS tests

--- build/task-test.js
+++ build/task-test.js
@@ -4,10 +4,26 @@ module.exports = function(grunt) {
   grunt.mergeConfig({
     jasmine: {
       'PLURALS': {
-        src: 'dist/standalone/Smart.Plurals.all.js'
+        src: [
+          'dist/standalone/Smart.Plurals.all.js'
+        ]
         , options: {
           specs: [
             'test/Smart/**/*.js'
+          ]
+        }
+      }
+      ,
+      'ANGULARJS': {
+        src: [
+          'bower_components/angular/angular.js'
+          , 'bower_components/angular-mocks/angular-mocks.js'
+
+          , 'dist/AngularJS/AngularJS.Smart.Plurals.all.js'
+        ]
+        , options: {
+          specs: [
+            'test/exports/AngularJS/**/*.js'
           ]
         }
       }
---
+++ test/exports/AngularJS/AngularJS.Smart.Plurals-Spec.js
@@ -0,0 +1,35 @@
+describe('AngularJS', function() {
+  describe('SmartPlurals service', function() {
+    beforeEach(module('smart'));
+
+    it('creates the SmartPlurals service', inject(function(SmartPlurals) {
+      expect(typeof SmartPlurals).toBe('object');
+      expect(typeof SmartPlurals.getRule).toBe('function');
+    }));
+  });
+  describe('plural filter', function() {
+    beforeEach(module('smart'));
+
+    it('creates the plural filter', inject(function(pluralFilter) {
+      expect(typeof pluralFilter).toBe('function');
+    }));
+
+    it('filters a number', inject(function($compile, $rootScope) {
+      var fixture = angular.element("<div> There {{ value | plural:'is':'are' }} {{ value }} {{ value | plural:'item':'items' }}. </div>");
+
+      // Bootstrap:
+      $compile(fixture)($rootScope);
+
+      $rootScope.$apply(function() {
+        $rootScope.value = 1;
+      });
+      expect(fixture.html()).toBe(" There is 1 item. ");
+
+      $rootScope.$apply(function() {
+        $rootScope.value = 2;
+      });
+      expect(fixture.html()).toBe(" There are 2 items. ");
+
+    }));
+  });
+});
