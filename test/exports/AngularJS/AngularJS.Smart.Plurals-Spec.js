d24d0ce02685bf576a3faa0885aafef64e9d7f97
Scott Rippey <scott.william.rippey@gmail.com>
2014-02-24 23:27:46 -0800

    Moved "inject" statements to a new line

--- test/exports/AngularJS/AngularJS.Smart.Plurals-Spec.js
+++ test/exports/AngularJS/AngularJS.Smart.Plurals-Spec.js
@@ -2,34 +2,41 @@ describe('AngularJS', function() {
   describe('SmartPlurals service', function() {
     beforeEach(module('smart'));
 
-    it('creates the SmartPlurals service', inject(function(SmartPlurals) {
-      expect(typeof SmartPlurals).toBe('object');
-      expect(typeof SmartPlurals.getRule).toBe('function');
-    }));
+    it('creates the SmartPlurals service',
+      inject(function(SmartPlurals) {
+        expect(typeof SmartPlurals).toBe('object');
+        expect(typeof SmartPlurals.getRule).toBe('function');
+      })
+    );
+
   });
   describe('plural filter', function() {
     beforeEach(module('smart'));
 
-    it('creates the plural filter', inject(function(pluralFilter) {
-      expect(typeof pluralFilter).toBe('function');
-    }));
+    it('creates the plural filter',
+      inject(function(pluralFilter) {
+        expect(typeof pluralFilter).toBe('function');
+      })
+    );
 
-    it('filters a number', inject(function($compile, $rootScope) {
-      var fixture = angular.element("<div> There {{ value | plural:'is':'are' }} {{ value }} {{ value | plural:'item':'items' }}. </div>");
+    it('filters a number',
+      inject(function($compile, $rootScope) {
+        var fixture = angular.element("<div> There {{ value | plural:'is':'are' }} {{ value }} {{ value | plural:'item':'items' }}. </div>");
 
-      // Bootstrap:
-      $compile(fixture)($rootScope);
+        // Bootstrap:
+        $compile(fixture)($rootScope);
 
-      $rootScope.$apply(function() {
-        $rootScope.value = 1;
-      });
-      expect(fixture.html()).toBe(" There is 1 item. ");
+        $rootScope.$apply(function() {
+          $rootScope.value = 1;
+        });
+        expect(fixture.html()).toBe(" There is 1 item. ");
 
-      $rootScope.$apply(function() {
-        $rootScope.value = 2;
-      });
-      expect(fixture.html()).toBe(" There are 2 items. ");
+        $rootScope.$apply(function() {
+          $rootScope.value = 2;
+        });
+        expect(fixture.html()).toBe(" There are 2 items. ");
 
-    }));
+      })
+    );
   });
 });
